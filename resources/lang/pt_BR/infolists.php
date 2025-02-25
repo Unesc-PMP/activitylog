9bae44bb612dba558e1830b5f2db301c408177af
Jorge <jorgeluixmp@unesc.net>
2025-02-25 14:31:40 -0300

    fix -> bugs & translation

--- resources/lang/pt_BR/infolists.php
+++ resources/lang/pt_BR/infolists.php
@@ -2,7 +2,7 @@
 
 return [
     'components' => [
-        "created_by_at" => "<strong>:causer</strong> <strong>:subject</strong> <strong>:event</strong>. <br><small> Atualizado em: <strong>:update_at</strong></small>",
+        "created_by_at" => "<strong>:causer</strong> <strong>:event</strong> <strong>:subject</strong>. <br><small> Atualizado em: <strong>:update_at</strong></small>",
         "updater_updated" => ":causer :event o seguinte: <br>:changes",
         "from_oldvalue_to_newvalue" => "-> <strong> ATUALIZAÇÃO: </strong> :key mudou de <strong>:old_value</strong> para <strong>:new_value</strong>",
         "to_newvalue" => "- :key <strong>:new_value</strong>",
